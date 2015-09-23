d789e4ec752da3d9309a5b38529f81739a967c91
Ernesto Herrera Salinas <ernestohs@gmail.com>
2015-09-23 17:07:22 -0500

    Add Fibonacci implementation

--- src/Fibonacci.cs
+++ src/Fibonacci.cs
@@ -2,5 +2,15 @@
 {
     public class Fibonacci
     {
+        private Func<int, int> fib;
+        
+        public object this[int nth]
+        {
+            get
+            {
+                fib = n => n > 1 ? fib(n - 1) + fib(n - 2) : n;
+                return fib( nth );
+            }
+        }
     }
 }
